34197645be5657246739f71bfcfa4edaecf8c226
airda93 <airda93@hotmail.com>
2013-12-09 20:31:08 +0100

    Update relacio.m

--- relacio.m
+++ relacio.m
@@ -1,3 +1,10 @@
+%The MIT License (MIT)
+
+%Copyright (c) 2013 Proyecto-GDSA Realized by: Adrià González, Nayara Ronldán, Mireia González, Helena Rodriguez
+% full description of the license is in the file LISENCE.TXT
+
+%-----------------------------------------------------------------------------------------------%
+
 function clase=relacio(pos,tags,c1,c2,c3,c4,c5,c6,c7,c8)
 switch pos
     case 1
