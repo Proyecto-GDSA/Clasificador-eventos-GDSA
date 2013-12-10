7e7e6495ea3c460834df4f1343a9419d9e192502
mire-53 <mireia.gonzalez.ferrer@gmail.com>
2013-12-10 11:49:57 +0100

    Update ralacio.m

--- ralacio.m
+++ ralacio.m
@@ -1,3 +1,17 @@
+%The MIT License (MIT)
+
+%Copyright (c) 2013 Proyecto-GDSA Realized by: Adrià González, Nayara Ronldán, Mireia González, Helena Rodriguez
+% full description of the license is in the file LISENCE.TXT
+
+%----------------------------------------------------------------------------------------------------------------%
+
+
+%This function relates the id of the new image with the corresponding class
+% pos --> is the position that relates the new image with the class that matches more
+% tags --> is the ID of new image
+% c1, c2, c3, c4, c5, c6, c7, c8 --> are sets of tags foreach image
+
+%__________________________________________________________________________________________________________________%
 
 function clase=relacio(pos,tags,c1,c2,c3,c4,c5,c6,c7,c8)
 switch pos
