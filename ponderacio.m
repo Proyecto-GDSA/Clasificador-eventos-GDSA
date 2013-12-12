0189575a5403c2f0b93d76b596e0b067ae8121bf
mire-53 <mireia.gonzalez.ferrer@gmail.com>
2013-12-12 18:36:04 +0100

    Update ponderacio.m

--- ponderacio.m
+++ ponderacio.m
@@ -1,3 +1,11 @@
+%The MIT License (MIT)
+
+%Copyright (c) 2013 Proyecto-GDSA Realized by: Adrià González, Nayara Roldán, Mireia González, Helena Rodriguez
+% full description of the license is in the file LISENCE.TXT
+
+%----------------------------------------------------------------------------------------------------------------%
+
+
 function idf=ponderacio(vector,map1,map2, map3, map4, map5, map6, map7)
 L=length(vector);
 control=0;
