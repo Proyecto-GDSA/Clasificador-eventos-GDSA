914d84d4ed54522176d056bbef0d1e2b0141e71f
mire-53 <mireia.gonzalez.ferrer@gmail.com>
2013-12-12 17:28:33 +0100

    Update buscarcoin.m

--- buscarcoin.m
+++ buscarcoin.m
@@ -11,7 +11,6 @@
 %________________________________________________________________________________________________________________%
 
 function solucio=buscarcoin(imag1,c1,c2,c3,c4,c5,c6,c7,c8)
-    L=length(imag1);
     L1=length(c1);
     L2=length(c2);
     L3=length(c3);
@@ -21,86 +20,24 @@ function solucio=buscarcoin(imag1,c1,c2,c3,c4,c5,c6,c7,c8)
     L7=length(c7);
     L8=length(c8);
 
-cont=0;    
-for i=2:L1
-    for j=2:L
-         sol=strcmp(c1(i),imag1(j));
-         if sol==1
-            cont=cont+1;
-         end
-    end
+cont1=coincidencias(c1,imag1);
+cont2=coincidencias(c2,imag1);
+cont3=coincidencias(c3,imag1);
+cont4=coincidencias(c4,imag1);
+cont5=coincidencias(c5,imag1);
+cont6=coincidencias(c6,imag1);
+cont7=coincidencias(c7,imag1);
+cont8=coincidencias(c8,imag1);
+
+
+cont1=cont1/L1;
+cont2=cont2/L2;
+cont3=cont3/L3;
+cont4=cont4/L4;
+cont5=cont5/L5;
+cont6=cont6/L6;
+cont7=cont7/L7;
+cont8=cont8/L8;
+
+solucion=[cont1,cont2,cont3,cont4,cont5,cont6,cont7,cont8];
 end
-
-cont1=0;
-for i=2:L2
-    for j=2:L
-        sol=strcmp(c2(i),imag1(j));
-        if sol==1
-            cont1=cont1+1;
-        end
-    end
-end  
-
-
-cont2=0;
-for i=2:L3
-    for j=2:L
-        sol=strcmp(c3(i),imag1(j));
-        if sol==1
-            cont2=cont2+1;
-        end
-    end
-end  
-
-cont3=0;
-for i=2:L4
-    for j=2:L
-        sol=strcmp(c4(i),imag1(j));
-        if sol==1
-            cont3=cont3+1;
-        end
-    end
-end 
-
-cont4=0;
-for i=2:L5
-    for j=2:L
-        sol=strcmp(c5(i),imag1(j));
-        if sol==1
-            cont4=cont4+1;
-        end
-    end
-end  
-
-cont5=0;
-for i=2:L6
-    for j=2:L
-        sol=strcmp(c6(i),imag1(j));
-        if sol==1
-            cont5=cont5+1;
-        end
-    end
-end  
-
-cont6=0;
-for i=2:L7
-    for j=2:L
-        sol=strcmp(c7(i),imag1(j));
-        if sol==1
-            cont6=cont6+1;
-        end
-    end
-end  
-
-cont7=0;
-for i=2:L8
-    for j=2:L
-        sol=strcmp(c8(i),imag1(j));
-        if sol==1
-            cont7=cont7+1;
-        end
-    end
-end  
-
-    
-solucio=[cont,cont1,cont2,cont3,cont4,cont5,cont6,cont7];
