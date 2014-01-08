5272d7a82f94bdf0a5a0d65a8940431f19fc1353
airda93 <airda93@hotmail.com>
2014-01-08 20:48:57 +0100

    Update buscarcoin.m

--- buscarcoin.m
+++ buscarcoin.m
@@ -10,7 +10,7 @@
 % c1, c2, c3, c4, c5, c6, c7, c8 --> are sets of tags for each image
 %________________________________________________________________________________________________________________%
 
-function solucio=buscarcoin(imag1,c1,c2,c3,c4,c5,c6,c7,c8)
+function solucio=buscarcoin(imag1,c1,c2,c3,c4,c5,c6,c7,c8,c9)
     L1=length(c1);
     L2=length(c2);
     L3=length(c3);
@@ -19,6 +19,7 @@ function solucio=buscarcoin(imag1,c1,c2,c3,c4,c5,c6,c7,c8)
     L6=length(c6);
     L7=length(c7);
     L8=length(c8);
+    L8=length(c9);
 
 cont1=coincidencias(c1,imag1);
 cont2=coincidencias(c2,imag1);
@@ -28,16 +29,17 @@ cont5=coincidencias(c5,imag1);
 cont6=coincidencias(c6,imag1);
 cont7=coincidencias(c7,imag1);
 cont8=coincidencias(c8,imag1);
+cont9=coincidencias(c9,imag1);
 
 
-cont1=cont1/L1;
-cont2=cont2/L2;
-cont3=cont3/L3;
-cont4=cont4/L4;
-cont5=cont5/L5;
-cont6=cont6/L6;
-cont7=cont7/L7;
-cont8=cont8/L8;
+% cont1=cont1/L1;
+% cont2=cont2/L2;
+% cont3=cont3/L3;
+% cont4=cont4/L4;
+% cont5=cont5/L5;
+% cont6=cont6/L6;
+% cont7=cont7/L7;
+% cont8=cont8/L8;
 
-solucion=[cont1,cont2,cont3,cont4,cont5,cont6,cont7,cont8];
+solucio=[cont1,cont2,cont3,cont4,cont5,cont6,cont7,cont8,cont9];
 end
