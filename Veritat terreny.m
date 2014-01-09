0fe95d78108b70abe058f25aa0a22537609aca8f
airda93 <airda93@hotmail.com>
2014-01-09 19:21:43 +0100

    Create Veritat terreny.m

---
+++ Veritat terreny.m
@@ -0,0 +1,37 @@
+%The MIT License (MIT)
+
+%Copyright (c) 2013 Proyecto-GDSA Realized by: Adrià González, Nayara Ronldán, Mireia González, Helena Rodriguez
+% full description of the license is in the file LISENCE.TXT
+
+%----------------------------------------------------------------------------------------------------------------%
+%this program serves for extract the truth ground for the avaluation
+
+
+fid=fopen('clases.txt');
+x=textscan(fid,'%s%s');
+fclose(fid);
+info=[x{1,1}, x{1,2}];
+
+seis=selecciona2num(info,'66');
+%seis=selecciona1(info,'6');
+%%
+siete=selecciona2num(info,'77');
+%siete=selecciona1(info,'7');
+%%
+ocho=selecciona2num(info,'88');
+%ocho=selecciona1(info,'8');
+%%
+nueve=selecciona2num(info,'99');
+%nueve=selecciona1(info,'9');
+%%
+zero=selecciona2num(info,'00');
+%zero=selecciona1(info,'0');
+%%
+tags2=[seis;siete];
+tags2=[tags2;nueve];
+tags2=[tags2;zero];
+tags2=separatags(tags2);
+
+
+
+save metadatosfotos00667799.mat tags2
